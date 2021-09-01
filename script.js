7fdb06545ba478e4d2c13f18a9a6940fc61bdeb8
Mimori256 <mimorimori256@gmail.com>
2021-09-01 22:09:35 +0900

    update

--- script.js
+++ script.js
@@ -51,6 +51,10 @@ const getGradeList = (courseList) => {
   return [ap, a, b, c, d, p, f, pending];
 };
 
+const filterByYear = (list, year) => {
+  return list.filter((course) => course[9] === year);
+};
+
 const getGp = (courseList) => {
   let gp = 0;
   let degree = 0;
@@ -142,29 +146,74 @@ const getGPA = (gp, degree) => {
   return gp / degree;
 };
 
+const createTable = (courseList) => {
+  const ref = document.getElementById("courseTable");
+  let table = document.createElement("table");
+  const gradeOrder = ["A+", "A", "B", "C", "D", "P", "F", "履修中"];
+  const gradeColor = [
+    "#FF9999",
+    "#FFCC99",
+    "#99CCFF",
+    "#FFFF99",
+    "#99FF99",
+    "#FFCCD8",
+    "#E599FF",
+    "#CCCCCC",
+  ];
+
+  //Sort course list
+  const compare = (a, b) => {
+    return gradeOrder.indexOf(a[7]) > gradeOrder.indexOf(b[7]);
+  };
+
+  courseList.sort(compare);
+
+  for (let i = 0; i < courseList.length; i++) {
+    let row = table.insertRow();
+
+    let cell1 = row.insertCell();
+    cell1.innerText = courseList[i][3];
+
+    let cell2 = row.insertCell();
+    cell2.innerText = courseList[i][7];
+
+    cell1.style.backgroundColor =
+      gradeColor[gradeOrder.indexOf(courseList[i][7])];
+    cell2.style.backgroundColor =
+      gradeColor[gradeOrder.indexOf(courseList[i][7])];
+  }
+
+  table.setAttribute("align", "center");
+
+  ref.appendChild(table);
+};
+
 form.seiseki.addEventListener("change", function (e) {
   const res = e.target.files[0];
   const reader = new FileReader();
   reader.readAsText(res);
   reader.addEventListener("load", function () {
     const data = formData(reader.result).split("\n");
-    const courseList = [];
-    const gpaCourseList = [];
+    let courseList = [];
+    let gpaCourseList = [];
+
+    //Check the selected year
+    let year = document.getElementById("nendo").value;
 
     //Get the values of the checkboxes
     const isGraphChecked = document.getElementById("graphCheck").checked;
     const isGpaChecked = document.getElementById("gpaCheck").checked;
 
     for (let i = 0; i < data.length; i++) {
-      if (data[i].split(",")[8] == "D" && isGraphChecked) {
+      if (data[i].split(",")[8] === "D" && isGraphChecked) {
         courseList.push(data[i].split(","));
       }
 
-      if (data[i].split(",")[8] == "D" && isGpaChecked) {
+      if (data[i].split(",")[8] === "D" && isGpaChecked) {
         gpaCourseList.push(data[i].split(","));
       }
 
-      if (data[i].split(",")[8] != "D") {
+      if (data[i].split(",")[8] !== "D") {
         courseList.push(data[i].split(","));
         gpaCourseList.push(data[i].split(","));
       }
@@ -174,17 +223,34 @@ form.seiseki.addEventListener("change", function (e) {
     courseList.shift();
     gpaCourseList.shift();
 
+    //Filtering by year
+    if (year !== "all") {
+      courseList = filterByYear(courseList, year);
+      gpaCourseList = filterByYear(gpaCourseList, year);
+    }
+
+    //Check if the course lists are not empty
+    try {
+      if (!courseList.length || !gpaCourseList.length) {
+        throw new Error("授業が存在しません");
+      }
+    } catch (e) {
+      alert(e.message);
+      document.location.reload();
+    }
+
     let degree = 0;
 
-    //Caluculate the number of degree
+    //Calculate the number of degree
     for (let i = 0; i < gpaCourseList.length; i++) {
-      if (["A+", "A", "B", "C", "D"].indexOf(gpaCourseList[i][7]) != -1) {
+      if (["A+", "A", "B", "C", "D"].indexOf(gpaCourseList[i][7]) !== -1) {
         degree = degree + Number(gpaCourseList[i][4]);
       }
     }
 
-    gradeList = getGradeList(courseList);
-    gp = getGp(gpaCourseList);
+    const gradeList = getGradeList(courseList);
+    const gp = getGp(gpaCourseList);
+
     createGraph(gradeList);
 
     const gpa =
@@ -194,7 +260,12 @@ form.seiseki.addEventListener("change", function (e) {
       " あなたのGPA(小数点第2位で四捨五入、P/F評価、履修中の科目は除外、教職以外のGPA対象外科目の除外には対応していません)";
     document.getElementById("message1").innerHTML = message1;
     document.getElementById("message2").innerHTML = message2;
-    document.getElementById("gpa").innerHTML = gpa;
+    document.getElementById("gpa").innerHTML = String(gpa);
     document.getElementById("selection").innerHTML = "";
+
+    //Create table
+    courseList.pop();
+    console.log(courseList);
+    createTable(courseList);
   });
 });
